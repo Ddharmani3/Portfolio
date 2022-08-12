940a43f8b9ff0eac2021a266bfe6d89313ef0714
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-13 02:38:20 +0530

    half navbar complete

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Insta from './insta'
+
+function navbar() {
+    return (
+        <div className='cont'>
+            <nav className='navbar'>
+                
+                {/* <img src="" alt="logo" className='logo' /> */}
+                
+                <ul className='navul'>
+                    <li className='navli'>Home</li>
+                    <li className='navli'>About</li>
+                    <li className='navli'>Portfolio</li>
+                    <li className='navli'>Contact</li>
+                </ul>
+
+             <ul className='social'>
+                <li><a target="_blank" href="https://www.instagram.com/ddharmani3/"> <Insta/></a></li>
+             </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default navbar
