5b4db0e694f6b6b29fb028f970c2a2a51afa5efe
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-14 19:01:01 +0530

    updated project js

--- src/components/project.js
+++ src/components/project.js
@@ -6,6 +6,21 @@ function project() {
     <div className='projects'>
       <div className="title">
         <h1 className='projtit'>PROJECTS</h1>
+        <div className="projlist">
+
+        <div className="proj">
+        <img src="proj1ss.png"id='proj2' alt="proj1ss" />
+        <h3>Portfloio Website</h3>
+        </div>
+        <div className="proj" id='proj2'>
+        <img src="proj1ss.png" alt="proj1ss" />
+        <h3>Chat App</h3>
+        </div>
+        <div className="proj">
+        <img src="proj1ss.png" id='proj3' alt="proj1ss" />
+        <h3>News WebApp</h3>
+        </div>
+        </div>
       </div>
     </div>
   )
