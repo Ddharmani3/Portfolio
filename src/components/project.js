3f1e4eec2cb919705f184ad3306adfbee8130d49
Hardik Dharmani <Ddharmani99@gmail.com>
2022-08-14 23:55:38 +0530

    improved icons

--- src/components/project.js
+++ src/components/project.js
@@ -19,7 +19,7 @@ function project() {
         <br />
           It is Made with Flutter and Firebase. </p>
           <div className="projuicon">
-            <img src="Reactlogo.png" width={"36px"}  alt="f" />
+            <img src="react.png" width={"36px"}  alt="f" />
             <img src="css3.png" width={"36px"} alt="" />
           </div>
           <div > <a className="gitsourcep" target="_blank" rel="noreferrer" href="https://github.com/Ddharmani3/Portfolio"><Github color="rgb(0 199 255)"/> <h3>View Source Code</h3></a></div>
@@ -55,7 +55,7 @@ function project() {
         <br />
           It is Made with Flutter and Firebase. </p>
           <div className="projuicon">
-            <img src="Reactlogo.png" width={"36px"} alt="" />
+            <img src="react.png" width={"36px"} alt="" />
             <img src="css3.png" width={"36px"} alt="" />
           </div>
           <div > <a className="gitsourcep" target="_blank" rel="noreferrer" href="https://github.com/Ddharmani3/News-Monkey"><Github color="rgb(0 199 255)"/> <h3>View Source Code</h3></a></div>
